fffb5e744d8992dc71233c437b4b659c07747843
ConRdav <connor@redwoodglade.com>
2022-01-21 02:38:26 +0000

    create validate_routine function to validate user routine input

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import re
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,15 +18,33 @@ three_day_routine = SHEET.worksheet('three')
 four_day_routine = SHEET.worksheet('four')
 five_day_routine = SHEET.worksheet('five')
 
+
 def get_workout_routine():
     """
     Gets the workout routine requested by the user
     """
     print("Please choose a workout routine.")
     print("Your options are a 3 day, 4 day or 5 day routine.")
-    print("Please choose your preferred routine by entering 3,4 or 5.\n")
+    print("Please choose your preferred routine by entering 3, 4 or 5.\n")
 
     routine = input("Enter your choice here: ")
-    print(f"You have chosen the {routine} day workout routine")
+    if validate_routine(routine):
+        print(f"You picked the {routine} day workout routine.")
+    else:
+        print("Invalid entry, please enter 3, 4 or 5.")
+
+    
+    
+
+def validate_routine(routine):
+    """
+    validates the user input
+    """
+    pattern = re.compile('[3-5]{1}')
+    return pattern.match(routine)
+
 
 get_workout_routine()
+
+        
+
